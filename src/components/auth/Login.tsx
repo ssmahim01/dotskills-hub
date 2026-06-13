393af7997b1c23c7fbd950c1dd63533418217065
ssmahim01 <mahimzayn666@gmail.com>
2026-06-13 22:21:28 +0600

    Updated user context

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -24,7 +24,6 @@ import {
 } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useAuthRedirect } from "@/lib/hooks/useAuthRedirect";
-import { useGetMyStoreQuery } from "@/redux/features/Store/store.api";
 
 const loginSchema = z.object({
   email: z
@@ -66,7 +65,6 @@ export default function Login() {
   const router = useRouter();
   const { login } = useUser();
   const { redirectAfterAuth } = useAuthRedirect();
-  const { data: myStore } = useGetMyStoreQuery();
 
   const {
     register,
@@ -83,12 +81,14 @@ export default function Login() {
     if (res.success) {
       login(res.user.user);
       toast.success("Welcome back!");
-      if (res.user.user.role === "OWNER") {
-        router.push(`/${myStore?.data?.slug}/dashboard`);
+      if (res.user.user.role === "CUSTOMER") {
+        router.push("/dashboard");
         redirectAfterAuth();
+
       } else if (res.user.user.role === "ADMIN") {
-        router.push("/admin");
+        router.push("/dashboard/admin");
         redirectAfterAuth();
+
       } else {
         router.push("/");
         redirectAfterAuth();
--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -4,6 +4,8 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { getCurrentUser } from "@/utils/getCurrentUser";
 import { logoutUser } from "@/utils/logoutUser";
+import { useRouter } from "next/navigation";
+import { useGetMyStoreQuery } from "@/redux/features/Store/store.api";
 
 type User = {
   _id: string;
@@ -23,6 +25,8 @@ const UserContext = createContext<UserContextType | null>(null);
 export function UserProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const { data: myStore } = useGetMyStoreQuery();
 
   useEffect(() => {
     const hydrateUser = async () => {
@@ -33,7 +37,11 @@ export function UserProvider({ children }: { children: React.ReactNode }) {
     hydrateUser();
   }, []);
 
-  const login = (userData: any) => setUser(userData);
+  const login = (userData: any) => {
+    setUser(userData);
+
+    router.push(`/${myStore?.data?.slug}/dashboard`);
+  };
   const logout = async () => {
     await logoutUser();
     setUser(null);
